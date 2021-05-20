e824828dd639b9e80d297c64b683df9cc991809f
Vishal8236 <vishalpatidar963027@gmail.com>
2021-05-20 17:04:33 +0530

    Image slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 
 function Home() {
 	return (
-		<div>
-			Home
-		</div>
+		<Container>
+			<ImgSlider />
+			<Viewers />
+		</Container>
 	)
 }
 
 export default Home
+
+const Container = styled.main`
+	min-height : calc(100vh - 70px);
+	padding : 0 calc(3.5vw + 5px);
+	position: relative;
+	overflow-x:hidden;
+
+	&:before{
+		background: url("/images/home-background.png") center center / cover no-repeat fixed;
+		position: absolute;
+		content : "";
+		left: 0;
+		right: 0;
+		bottom: 0;
+		top : 0;
+		z-index: -1;
+	}
+`
+
